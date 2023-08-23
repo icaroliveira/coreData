b9c36b271f32a6f24974906219adf38c6e496691
icaro rangel <icarogin2@gmail.com>
2023-08-23 17:39:34 -0400

    adicao do coredata, alerts e algumas outras alteracoes

--- NameList/NameList/ViewController.swift
+++ NameList/NameList/ViewController.swift
@@ -6,17 +6,35 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var names: [String] = []
+    var people: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Lista de Nomes"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do{
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Nao foi possivel retornar os registros \(error)")
+        }
+    }
+    
     @IBAction func addName(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Nome", message: "Insira o novo nome", preferredStyle: .alert)
         
@@ -27,7 +45,8 @@ class ViewController: UIViewController {
             let nameToSave = textField.text else {
                 return
             }
-            self.names.append(nameToSave)
+            
+            save(name: nameToSave)
             self.tableView.reloadData()
         }
         let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel)
@@ -39,17 +58,35 @@ class ViewController: UIViewController {
         present(alert, animated: true)
         
     }
-    
+    func save(name: String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        let person  = NSManagedObject(entity: entity, insertInto: managedContext)
+        person.setValue(name, forKey: "name")
+        
+        do{
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError{
+            print("Erro ao salvar novo nome\(error)")
+        }
+        
+    }
 }
 
 extension ViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let person = people[indexPath.row]
         
-        cell.textLabel?.text = names [indexPath.row]
+        cell.textLabel?.text = person.value(forKey: "name") as? String
         return cell
     }
 }
